c9faf0b4613291b9258a1863441e48b438cd29a1
Login <loginfunboy@gmail.com>
2025-12-19 12:08:58 +0530

    Fix CORS to allow frontend domain

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://meditation-timer-1q8rilsbo-login-ss-projects.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Database connection
